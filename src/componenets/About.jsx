97f8eafa29739fba21bdd59a3264752aa92ef537
Pezhman <33788011+priestmoon50@users.noreply.github.com>
2020-08-05 16:46:09 +0430

    Update
    Replace <a> tag with
    <link to>

--- src/componenets/About.jsx
+++ src/componenets/About.jsx
@@ -2,21 +2,22 @@ import React from "react";
 
 
 import "./about.css"
+import { Link } from "react-router-dom";
 export default function About() {
   return (
     <div className="body">
       <h1>Hello from About</h1>
       <div class="navbar">Social 
         <ul class="menu">
-          <li><a href="#" class="fa fa-facebook"> </a></li>
-          <li><a href="#" class="fa fa-instagram"> </a></li>
-          <li><a href="#" class="fa fa-google"> </a></li>
-          <li><a href="#" class="fa fa-twitch"> </a></li>
-          <li><a href="#" class="fa fa-whatsapp"> </a></li>
-          <li><a href="#" class="fa fa-telegram"> </a></li>
-          <li><a href="#" class="fa fa-linkedin"> </a></li>
-          <li><a href="#" class="fa fa-twitter"> </a></li>
-          <li><a href="#" class="fa fa-github"> </a></li>
+          <li><Link to="#" class="fa fa-facebook"> </Link></li>
+          <li><Link to="#" class="fa fa-instagram"> </Link></li>
+          <li><Link to="#" class="fa fa-google"> </Link></li>
+          <li><Link to="#" class="fa fa-twitch"> </Link></li>
+          <li><Link to="#" class="fa fa-whatsapp"> </Link></li>
+          <li><Link to="#" class="fa fa-telegram"> </Link></li>
+          <li><Link to="#" class="fa fa-linkedin"> </Link></li>
+          <li><Link to="#" class="fa fa-twitter"> </Link></li>
+          <li><Link to="#" class="fa fa-github"> </Link></li>
         </ul>
       </div>
     </div>
