837f122ab1718e839984803185872c4024f3eb3d
Pezhman <33788011+priestmoon50@users.noreply.github.com>
2020-08-05 16:41:27 +0430

    Social links
    About ro check kon bebin khobe

--- src/componenets/About.jsx
+++ src/componenets/About.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
+
+import "./about.css"
 export default function About() {
   return (
-    <div>
+    <div className="body">
       <h1>Hello from About</h1>
+      <div class="navbar">Social 
+        <ul class="menu">
+          <li><a href="#" class="fa fa-facebook"> </a></li>
+          <li><a href="#" class="fa fa-instagram"> </a></li>
+          <li><a href="#" class="fa fa-google"> </a></li>
+          <li><a href="#" class="fa fa-twitch"> </a></li>
+          <li><a href="#" class="fa fa-whatsapp"> </a></li>
+          <li><a href="#" class="fa fa-telegram"> </a></li>
+          <li><a href="#" class="fa fa-linkedin"> </a></li>
+          <li><a href="#" class="fa fa-twitter"> </a></li>
+          <li><a href="#" class="fa fa-github"> </a></li>
+        </ul>
+      </div>
     </div>
   );
 }
